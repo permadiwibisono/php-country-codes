0cd1994c171d1c66046e38726819daa3bf6d9bab
Permadi Wibisono <mbapewe@gmail.com>
2019-05-21 04:54:00 +0700

    Added params --limit and --offset

--- src/examples/get_country_list.php
+++ src/examples/get_country_list.php
@@ -4,8 +4,33 @@ require('Country.php');
 $country_wrapper = new Pewe\CountryCodes\Country();
 
 $list = $country_wrapper->getList();
-
-foreach ($list as $key => $item) {
-	echo("Country iso2: $key\n");
+$limit = count($list);
+$offset = 0;
+if(count($argv) > 1){
+	$values = get_inputs(array_slice($argv, 1), ['offset' => $offset, 'limit' => $limit]);
+	$offset = $values['offset'];
+	$limit = $values['limit'];
+}
+$from = $offset + 1;
+$total = $offset + $limit;
+echo("Get country code data from $from to $total...\n");
+foreach (array_slice($list, $offset, $limit) as $key => $item) {
+	echo("Country iso2 code --> $key\n");
 	print_r($item);
+}
+
+function get_inputs($inputs, $initial_values)
+{
+	$input_accepted = [ 'offset' => '--offset=', 'limit' => '--limit='];
+	$values = $initial_values;
+	foreach ($inputs as $input) {
+		foreach ($input_accepted as $key => $value) {
+			$regex = "/^$value/";
+			if(preg_match($regex, $input)){
+				$values[$key] = preg_replace($regex, "", $input);
+				break;
+			}
+		}
+	}
+	return $values;
 }
